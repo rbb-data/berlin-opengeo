8a0028564acd6b91b91a6398deb9f38d37cf949f
Arne Schlüter <aesthaddicts@gmail.com>
2017-01-12 14:05:33 +0100

    Adjust setup script to work on OSX and use compressed csv data instead of downloading the complete JSON dump

--- setup.sh
+++ setup.sh
@@ -1,34 +1,21 @@
-#! /bin/sh
+#!/bin/bash
 set -e
-
-URL="http://download.odcdn.de/"
-NAME="full-data.json.stream"
-EXTENSION=".7z"
-DEST="./data"
-
-echo "Creating the destination directory..."
-if [ ! -d "$DEST" ]; then
-  mkdir $DEST
-fi
-
-echo "Downloading the file"
-wget -O $DEST/$NAME$EXTENSION $URL$NAME$EXTENSION
-
-echo "Extracting the archive"
-cd $DEST
-7z e $NAME$EXTENSION
 
 # if your mongodb stores data in a folder with to few memory, please chose another one using:
 # mongod --dbpath /lot/of/freespace
-FREE=$(df -h ./ | grep -vE '^Filesystem|tmpfs'| awk '{ print $4 }' | sed 's/G//')
-if [ $FREE -lt 500 ]; then
+FREE=$(df -h ./ | grep -vE '^Filesystem|tmpfs'| awk '{ print $4 }' | sed -E 's/Gi//')
+if [ $FREE -lt 5 ]; then
 	echo "There are less than 5 GB freespace"
 	echo "Please choose another mongopath:"
 	echo "mongod --dbpath /lot/of/freespace"
 fi
 
-mongoimport --db geocoder --collection data < data/full-data.json.stream
+# get field names from one csv, import data via stream
+# conversion recipe inspired by https://thedatachef.blogspot.de/2011/01/convert-tsv-to-json-command-line.html
+echo "Converting data.tsv.bz2 to json"
+FIELDS=$(cat ../data/headers_data_tsv.csv | head -n 1 | sed $'s/\t/,/g')
+bzip2 -d ../data/data.tsv.bz2 --stdout \
+  | ruby -rjson -ne 'puts ENV["FIELDS"].split(",").zip($_.strip.split("\t")).inject({}){|h,x| h[x[0]]=x[1];h}.to_json' \
+  | mongoimport --db geocoder --collection data
 
-echo "Clean the destination directory"
-cd ..
-rm -r $DEST
+echo "Done!"
